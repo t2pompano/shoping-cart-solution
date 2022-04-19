ddce394dd8f66825b55a229a165f04155a39090d
t2pompano <t2pompano@gmail.com>
2022-04-19 00:38:23 -0400

    case function done

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('I am in a separate file')
+function caseUpdateNumber(isincreasing) {
+    const caseInput = document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+    if (isincreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+
+}
+// handle phone increase decrease event
+document.getElementById('phone-plus').addEventListener('click', function () {
+    console.log('phone plus clicked');
+})
+
+
+// handle case increase decrease event
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    caseUpdateNumber(true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    // const caseInput = document.getElementById('case-number');
+    // const caseNumber = caseInput.value;
+    caseUpdateNumber(false);
+
+});
